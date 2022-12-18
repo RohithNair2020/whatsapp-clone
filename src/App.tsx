79b4691dc655fa732ea3807ace9d95b69f3f4917
Rohith <rohith4518@gmail.com>
2022-12-18 16:18:07 +0530

    added and configured eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-function App() {
-  return <div className="App">Whatsapp Clone</div>;
+interface Props {
+    name?: string;
 }
+
+const App = (props: Props) => {
+    const { name } = props;
+    console.log(name);
+    return <div className="App'">Whatsapp Clone</div>;
+};
+
+App.defaultProps = {
+    name: 'Whatsapp Loading',
+};
 
 export default App;

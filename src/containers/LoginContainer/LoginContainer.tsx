c2bdca19a5f742258422bd8b144ad0d80ee87e84
Rohith <rohith4518@gmail.com>
2022-12-27 16:29:47 +0530

    added formik to Login page

--- src/containers/LoginContainer/LoginContainer.tsx
+++ src/containers/LoginContainer/LoginContainer.tsx
@@ -1,38 +1,33 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
-    Formik, Field, Form, FormikHelpers,
+    Formik, Field, Form,
 } from 'formik';
 import API from '../../Router/api';
 import useStore, { Store } from '../../Store';
 import './loginContainer.css';
 
 interface Values {
-    firstName: string;
-    lastName: string;
-    email: string;
+    phone: string;
+    password: string;
 }
+
+const initialValues = {
+    phone: '',
+    password: '',
+};
 
 const LoginContainer = () => {
     console.log('props');
-    const [phone, setPhone] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
     const setUser = useStore((state: Store) => state.setUser);
     const navigate = useNavigate();
 
-    const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log('login event', e.target.value);
-        setPhone(e.target.value);
-    };
-
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log('login event', e);
-        setPassword(e.target.value);
-    };
-
-    const login = async () => {
-        const response = await axios.post(API.LOGIN, { phone, password });
+    const login = async (values: Values) => {
+        const response = await axios.post(API.LOGIN, {
+            phone: values.phone,
+            password: values.password,
+        });
         console.log('login response', response);
         if (response.data) {
             localStorage.setItem('auth', response.data.token);
@@ -51,72 +46,34 @@ const LoginContainer = () => {
     return (
         <div className="login-container">
             <h2 className="login-title">Login</h2>
-            <div className="login-form-container">
-                <div className="login-form">
-                    <div className="login-input-container">
-                        <label htmlFor="login-num">Phone (+91)</label>
-                        <input
-                            onChange={handlePhoneChange}
-                            type="tel"
-                            value={phone}
-                            className="login-input"
-                        />
-                    </div>
-                    <div className="login-input-container">
-                        <label htmlFor="login-pass">Password</label>
-                        <input
-                            onChange={handlePasswordChange}
-                            type="password"
-                            value={password}
-                            className="login-input"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="login-button"
-                        onClick={login}
-                    >
-                        Login
-                    </button>
-                    <Link to="/register" className="link">
-                        <i className="register-link">Create an account</i>
-                    </Link>
-                </div>
-            </div>
             <div>
-                <h1>Signup</h1>
                 <Formik
-                    initialValues={{
-                        firstName: '',
-                        lastName: '',
-                        email: '',
-                    }}
-                    onSubmit={(
-                        values: Values,
-                        { setSubmitting }: FormikHelpers<Values>,
-                    ) => {
-                        setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            setSubmitting(false);
-                        }, 500);
-                    }}
+                    initialValues={initialValues}
+                    onSubmit={login}
                 >
                     <Form>
-                        <label htmlFor="firstName">First Name</label>
-                        <Field id="firstName" name="firstName" placeholder="John" />
-
-                        <label htmlFor="lastName">Last Name</label>
-                        <Field id="lastName" name="lastName" placeholder="Doe" />
-
-                        <label htmlFor="email">Email</label>
-                        <Field
-                            id="email"
-                            name="email"
-                            placeholder="john@acme.com"
-                            type="email"
-                        />
-
-                        <button type="submit">Submit</button>
+                        <div className="registration-form-container">
+                            <div className="registration-form">
+                                <div className="registration-input-container">
+                                    <label htmlFor="phone">phone</label>
+                                    <Field className="registration-input" id="phone" name="phone" placeholder="Enter Contact No" />
+                                </div>
+                                <div className="registration-input-container">
+                                    <label htmlFor="password">Password</label>
+                                    <Field
+                                        className="registration-input"
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Enter Password"
+                                    />
+                                </div>
+                                <button className="registration-button" type="submit">Login</button>
+                                <Link to="/login" className="link">
+                                    <i className="register-link">Already have an account</i>
+                                </Link>
+                            </div>
+                        </div>
                     </Form>
                 </Formik>
             </div>
